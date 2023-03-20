a5a6f5b435d77c1a829e2be973d587b06e16944d
Tom <tom.dalrymple3@gmail.com>
2023-03-20 11:40:54 +0000

    added the rest of the classes

--- src/Star.java
+++ src/Star.java
@@ -1,4 +1,23 @@
 public class Star {
     private double GHA;
     private double declination;
+    private double GP;
+    private String name;
+
+    public Star(String name) {
+        this.declination = declinationFromAlmanac(name);
+        this.GHA = GHAFromAlmanac(name);
+    }
+
+    private double declinationFromAlmanac(String star) {
+
+    }
+
+    private double GHAFromAlmanac(String star) {
+
+    }
+
+    public double GP() {
+
+    }
 }
