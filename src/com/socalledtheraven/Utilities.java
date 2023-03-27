5cda0dc71d549c7fe21ac2fa711b32734337bb0d
Tom <tom.dalrymple3@gmail.com>
2023-03-27 11:45:49 +0100

    start on file processing

--- src/com/socalledtheraven/Utilities.java
+++ src/com/socalledtheraven/Utilities.java
@@ -1,7 +1,13 @@
 package com.socalledtheraven;
+import
 
 public class Utilities {
     public static double angleToNauticalMiles(double angles) {
         return angles*60;
+    }
+
+    public static void processAlmanac() {
+        PDDocument document = PDDocument.load(new File("example.pdf"));
+
     }
 }
